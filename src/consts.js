91969f4f316c81c9e98f1966fe6bc183a1fd82f1
Jarne <jarne.demeyer05@hotmail.com>
2025-03-01 21:31:41 +0100

    add files

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,6 @@
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const SOURCE_PATH = path.resolve("src");
 export const VIEWS_PATH = path.resolve(SOURCE_PATH, "views");
